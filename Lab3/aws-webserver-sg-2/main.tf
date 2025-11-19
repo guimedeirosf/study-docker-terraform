d02ded699769e6a458b7fd77149fe050499585be
Guilherme Medeiros <grmfurquim@gmail.com>
2025-11-18 23:54:40 -0300

    Copiando o comando bash via arquivo e depois executando

---
+++ Lab3/aws-webserver-sg-2/main.tf
@@ -0,0 +1,67 @@
+resource "aws_security_group" "permite_ssh_2" {
+  
+  name = "permite_ssh_2"
+  
+  description = "Security Group EC2 Instance"
+
+  ingress {
+
+    description = "Inbound Rule"
+    from_port = 22
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+
+    description = "Outbound Rule"
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    }
+
+}
+
+resource "aws_instance" "grmf_instance" {
+  
+  ami = "ami-0a0d9cf81c479446a"  
+  
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [aws_security_group.permite_ssh_2.id]
+
+  key_name = "grmf-lab3"
+
+
+  tags = {
+    Name = "lab3-t3-terraform"
+  }
+
+  provisioner "file" {
+    source      = "grmf_script.sh"
+    destination = "/tmp/grmf_script.sh"
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      private_key = file("grmf-lab3.pem")
+      host     = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    
+    inline = ["chmod +x /tmp/grmf_script.sh", "/tmp/grmf_script.sh"]
+
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      private_key = file("grmf-lab3.pem")
+      host     = self.public_ip
+    }
+  }
+}
---
+++ Lab3/aws-webserver-sg-2/variables.tf
@@ -0,0 +1,4 @@
+variable "instance_type" {
+  description = "Tipo de instância EC2."
+  default     = "t3.micro"
+}
